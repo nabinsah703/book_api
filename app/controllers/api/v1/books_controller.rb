c2089e79a24b29d901db95b898718e554fd7ea3f
Nabin Sah <nabinsah703@gmail.comgit config --global user.email nabinsah703@gmail.com>
2022-04-06 12:30:46 +0545

    i create a book api

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,29 @@
+module Api
+    module V1
+
+
+        class BooksController < ApplicationController
+            ALLOWED_DATA = %[title description rating].freeze    
+            def index
+                books = Book.all
+                render json: books 
+            end
+
+            def show
+                book =Book.find(params[:id])
+                render json: book
+            end
+
+            def create
+                data = json_payload.select {|k| ALLOWED_DATA.include?(k)}
+                book = Book.new(data)
+                if book.save
+                    render json: book
+                else
+                    render json: {"error":"Couldnot create it"}
+                end
+            end
+        end
+    end
+end
+
